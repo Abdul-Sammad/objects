6638eda970569b002ec77d87b1b94e60e0bfbf8c
Abdul-Sammad <100076834+Abdul-Sammad@users.noreply.github.com>
2022-06-10 20:31:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,94 @@
+//question no 1
+// const sumTotal = arr =>
+//   arr.reduce((sum, { price, quantity }) => sum + price * quantity, 0)
+
+// const data = [
+//   { id: 1, price: 30, quantity: 2 },
+//   { id: 2, price: 20, quantity: 4 },
+//   { id: 3, price: 10, quantity: 2 },
+// ]
+
+// const total = sumTotal(data)
+
+// console.log(total) // 160
+
+
+
+//question no 2
+// var person = {
+//     name: "sammad",
+//     age: 18,
+//     gender: "Male",
+//     displayName: function() {
+//         alert(this.name);
+//     }
+// };
+
+// document.write(person.name + "<br>"); 
+// document.write(person.age + "<br>"); 
+// document.write(person.gender); 
+// console.log(person); 
+
+
+
+
+//question no 3
+// constructor function
+// function Person () {
+//     this.name = 'John',
+//     this.age = 23
+// }
+
+// // create an object
+// const person = new Person();
+
+// // constructor function
+// function Person () {
+//     this.name = 'John',
+//     this.age = 23,
+
+//      this.greet = function () {
+//         console.log('hello');
+//     }
+// }
+
+// // create objects
+// const person1 = new Person();
+// const person2 = new Person();
+
+// // access properties
+// console.log(person1.name);  // John
+// console.log(person2.name);  // John
+
+
+// // constructor function
+// function Person () {
+//     this.name = 'John',
+// }
+
+// // create object
+// const person1 = new Person();
+
+// // access properties
+// console.log(person1.name);  // John
+
+// // constructor function
+// function Person (person_name, person_age, person_gender) {
+
+//     // assigning  parameter values to the calling object
+//      this.name = person_name,
+//      this.age = person_age,
+//      this.gender = person_gender,
+ 
+//      this.greet = function () {
+//          return ('Hi' + ' ' + this.name);
+//      }
+//  }
+ 
+ 
+//  // creating objects
+//  const person1 = new Person('John', 23, 'male');
+//  const person2 = new Person('Sam', 25, 'female');
+ 
+
+ 
